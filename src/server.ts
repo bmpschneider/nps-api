2c78a901a0eb2cfa402dc115c4f388c613389fd6
bmpschneider <brunopschneider@gmail.com>
2021-02-23 20:11:35 -0300

    Insert Controller and Model

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import 'reflect-metadata';
 import express, { response } from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(router);
 
-app.get('/', (req, res) => {
-  return res.send('Hello World!');
-});
-
-app.post('/', (req, res) => {
-  return res.json({ message: "Save OK"});
-});
 
 app.listen(3000, () => console.log('Server is running!'));
